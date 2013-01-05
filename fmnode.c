66b6e78ed5815401870f8e2f30579a86842653cc
Olivier Brunel <jjk@jjacky.com>
2013-01-05 15:41:52 +0100

    work on new interfaces

---
+++ fmnode.c
@@ -0,0 +1,470 @@
+
+typedef struct _FmProvider FmProvider;
+#define IS_FMPROVIDER(o)    NULL
+
+#include <gtk/gtk.h>
+#include <gobject/gvaluecollector.h>    /* G_VALUE_LCOPY */
+#include <string.h>                     /* memset() */
+#include "fmnode.h"
+
+struct _FmNodePrivate
+{
+    FmProvider  *provider;
+    gchar       *location;
+    GMutex       location_mutex;
+    GHashTable  *props;
+    GRWLock      props_lock;
+};
+
+typedef struct
+{
+    /* name is the key in the hash table */
+    GValue       value;
+    gboolean     has_value; /* is value set, or do we need to call get_value? */
+    get_value_fn get_value;
+    set_value_fn set_value;
+} FmNodeProp;
+
+static void fmnode_finalize (GObject *object);
+
+static void
+fmnode_class_init (FmNodeClass *klass)
+{
+    GObjectClass *o_class;
+
+    o_class = G_OBJECT_CLASS (klass);
+    o_class->finalize = fmnode_finalize;
+    g_type_class_add_private (klass, sizeof (FmNodePrivate));
+}
+
+static void
+fmnode_init (FmNode *node)
+{
+    node->priv = G_TYPE_INSTANCE_GET_PRIVATE (node,
+            TYPE_FMNODE,
+            FmNodePrivate);
+}
+
+G_DEFINE_TYPE (FmNode, fmnode, G_TYPE_OBJECT)
+
+static void
+fmnode_finalize (GObject *object)
+{
+    FmNodePrivate *priv;
+
+    priv = FMNODE (object)->priv;
+    /* it is said that dispose should do the unref-ing, but at the same time
+     * the object is supposed to be able to be "revived" from dispose, and we
+     * need a ref to provider to survive... */
+    g_object_unref (priv->provider);
+    g_free (priv->location);
+    g_mutex_clear (&priv->location_mutex);
+    g_hash_table_destroy (priv->props);
+    g_rw_lock_clear (&priv->props_lock);
+
+    G_OBJECT_CLASS (fmnode_parent_class)->finalize (object);
+}
+
+/* used to free properties when removed from hash table */
+static void
+free_prop (gpointer prop)
+{
+    g_slice_free (FmNodeProp, prop);
+}
+
+FmNode *
+fmnode_new (FmProvider  *provider,
+            const gchar *location,
+            gboolean     is_container)
+{
+    FmNode *node;
+    FmNodePrivate *priv;
+
+    g_return_val_if_fail (IS_FMPROVIDER (provider), NULL);
+    g_return_val_if_fail (location != NULL, NULL);
+
+    node = g_object_new (TYPE_FMNODE, NULL);
+    priv = node->priv;
+    priv->provider = provider;
+    priv->location = g_strdup (location);
+    g_mutex_init (&priv->location_mutex);
+    priv->props = g_hash_table_new_full (g_str_hash, g_str_equal,
+            g_free, free_prop);
+    g_rw_lock_init (&priv->props_lock);
+
+    g_object_ref (provider);
+
+    return node;
+}
+
+FmNode *
+fmnode_new_from_node (FmProvider    *provider,
+                      const gchar   *location,
+                      gboolean       is_container,
+                      FmNode        *sce)
+{
+    FmNode *node;
+    GHashTable *props;
+    GHashTableIter iter;
+    gpointer key, value;
+
+    g_return_val_if_fail (IS_FMNODE (sce), NULL);
+
+    /* create a new node */
+    node = fmnode_new (provider, location, is_container);
+
+    g_return_val_if_fail (IS_FMNODE (node), NULL);
+
+    /* and copy over all the properties */
+    props = node->priv->props;
+    g_rw_lock_reader_lock (&sce->priv->props_lock);
+    g_hash_table_iter_init (&iter, sce->priv->props);
+    while (g_hash_table_iter_next (&iter, &key, &value))
+    {
+        FmNodeProp *prop;
+        FmNodeProp *prop_sce = value;
+
+        prop = g_slice_copy (sizeof (*prop), prop_sce);
+        /* for the GValue we'll need to reset the memory, re-init it */
+        memset (&prop->value, 0, sizeof (GValue));
+        g_value_init (&prop->value, G_VALUE_TYPE (&prop_sce->value));
+        /* and if there's a value, re-copy it over */
+        if (prop->has_value)
+            g_value_copy (&prop_sce->value, &prop->value);
+
+        g_hash_table_insert (props, (gpointer) g_strdup (key), value);
+    }
+    g_rw_lock_reader_unlock (&sce->priv->props_lock);
+
+    return node;
+}
+
+FmProvider *
+fmnode_get_provider (FmNode *node)
+{
+    g_return_val_if_fail (IS_FMNODE (node), NULL);
+    return node->priv->provider;
+}
+
+gchar *
+fmnode_get_location (FmNode *node)
+{
+    gchar *loc;
+
+    g_return_val_if_fail (IS_FMNODE (node), NULL);
+
+    g_mutex_lock (&node->priv->location_mutex);
+    loc = g_strdup (node->priv->location);
+    g_mutex_unlock (&node->priv->location_mutex);
+    return loc;
+}
+
+gboolean
+fmnode_add_property (FmNode          *node,
+                     const gchar     *name,
+                     GType            type,
+                     GValue          *value,
+                     get_value_fn     get_value,
+                     set_value_fn     set_value,
+                     GError         **error)
+{
+    FmNodePrivate   *priv;
+    FmNodeProp      *prop;
+
+    g_return_val_if_fail (IS_FMNODE (node), FALSE);
+    g_return_val_if_fail (name != NULL, FALSE);
+    g_return_val_if_fail (get_value != NULL, FALSE);
+    /* set_value is optional (can be read-only) */
+
+    priv = node->priv;
+    g_rw_lock_writer_lock (&priv->props_lock);
+    if (g_hash_table_contains (priv->props, name))
+    {
+        g_rw_lock_writer_unlock (&priv->props_lock);
+        g_set_error (error, FMNODE_ERROR, FMNODE_ERROR_ALREADY_EXISTS,
+                "Node already contains a property %s", name);
+        return FALSE;
+    }
+    /* allocate a new FmNodeProp to hold the property value */
+    prop = g_slice_new0 (FmNodeProp);
+    prop->get_value = get_value;
+    prop->set_value = set_value;
+    /* init the GValue */
+    g_value_init (&prop->value, type);
+    /* do we have an init value to set? */
+    if (value != NULL)
+    {
+        if (G_VALUE_HOLDS (value, type))
+        {
+            g_value_copy (value, &prop->value);
+            prop->has_value = TRUE;
+        }
+        else
+        {
+            g_rw_lock_writer_unlock (&priv->props_lock);
+            g_set_error (error, FMNODE_ERROR, FMNODE_ERROR_INVALID_TYPE,
+                    "Invalid format for initial value of new property %s: "
+                    "property is %s, initial value is %s",
+                    name,
+                    g_type_name (type),
+                    g_type_name (G_VALUE_TYPE (&prop->value)));
+            g_slice_free (FmNodeProp, prop);
+            return FALSE;
+        }
+    }
+    /* add prop to the hash table */
+    g_hash_table_insert (priv->props, (gpointer) g_strdup (name), prop);
+    g_rw_lock_writer_unlock (&priv->props_lock);
+
+    return TRUE;
+}
+
+static gboolean
+set_property_checks (FmNode          *node,
+                     const gchar     *name,
+                     GValue          *value,
+                     GError         **error,
+                     FmNodeProp     **prop)
+{
+    FmNodePrivate *priv;
+
+    g_return_val_if_fail (IS_FMNODE (node), FALSE);
+    g_return_val_if_fail (name != NULL, FALSE);
+    g_return_val_if_fail (value != NULL, FALSE);
+
+    priv = node->priv;
+    g_rw_lock_reader_lock (&priv->props_lock);
+    *prop = g_hash_table_lookup (priv->props, (gpointer) name);
+    /* the lock is for the hash table only. the FmNodeProp isn't going anywhere,
+     * nor can it change, so we can let it go now. The only thing that can
+     * happen is a change of value, but the type will/can not change */
+    g_rw_lock_reader_unlock (&priv->props_lock);
+    if (!*prop)
+    {
+        g_set_error (error, FMNODE_ERROR, FMNODE_ERROR_NOT_FOUND,
+                "Node does not have a property %s", name);
+        return FALSE;
+    }
+
+    if (!(*prop)->set_value)
+    {
+        g_set_error (error, FMNODE_ERROR, FMNODE_ERROR_READ_ONLY,
+                "Property %s on node cannot be set", name);
+        return FALSE;
+    }
+
+    if (!G_VALUE_HOLDS (value, G_VALUE_TYPE (&((*prop)->value))))
+    {
+        g_set_error (error, FMNODE_ERROR, FMNODE_ERROR_INVALID_TYPE,
+                "Property %s on node is of type %s, value passed is %s",
+                name,
+                g_type_name (G_VALUE_TYPE (&((*prop)->value))),
+                g_type_name (G_VALUE_TYPE (value)));
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+gboolean
+fmnode_set_property (FmNode          *node,
+                     const gchar     *name,
+                     GValue          *value,
+                     GError         **error)
+{
+    GError *err = NULL;
+    FmNodeProp *prop;
+
+    if (!set_property_checks (node, name, value, error, &prop))
+        return FALSE;
+
+    /* no lock, because the provider/whoever will do the work might take a
+     * while (slow fs, network connection, some timing out...) and during this
+     * time, no need to keep a lock for nothing.
+     * The callback (set_value) will use set_property_value to update value(s),
+     * which will use a writer lock of course */
+    if (!prop->set_value (node, name, value, &err))
+    {
+        g_propagate_error (error, err);
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+static void
+set_property (FmNode             *node,
+              const gchar        *name,
+              GValue             *value,
+              GCancellable       *cancellable,
+              GSimpleAsyncResult *result)
+{
+    GError *err = NULL;
+    FmNodeProp *prop;
+    FmNodePrivate *priv;
+    gboolean ret;
+
+    priv = node->priv;
+    g_rw_lock_reader_lock (&priv->props_lock);
+    prop = g_hash_table_lookup (priv->props, (gpointer) name);
+    g_rw_lock_reader_unlock (&priv->props_lock);
+
+    ret = prop->set_value (node, name, value, &err);
+    if (!ret)
+        g_simple_async_result_take_error (result, err);
+    g_simple_async_result_set_op_res_gboolean (result, ret);
+    g_simple_async_result_complete_in_idle (result);
+    g_object_unref (result);
+}
+
+void
+fmnode_set_property_async (FmNode                 *node,
+                           const gchar            *name,
+                           GValue                 *value,
+                           GCancellable           *cancellable,
+                           GAsyncReadyCallback     callback,
+                           gpointer                data)
+{
+    GSimpleAsyncResult *result;
+    GError *err = NULL;
+    FmNodeProp *prop;
+
+    if (!set_property_checks (node, name, value, &err, &prop))
+    {
+        g_simple_async_report_take_gerror_in_idle (G_OBJECT (node),
+                callback,
+                data,
+                err);
+        return;
+    }
+
+    result = g_simple_async_result_new (G_OBJECT (node),
+            callback,
+            data,
+            fmnode_set_property_async);
+    start_in_thread (set_property, node, name, value, cancellable, result);
+}
+
+gboolean
+fmnode_set_property_finish (FmNode                 *node,
+                            GAsyncResult           *result,
+                            GError                **error)
+{
+    GSimpleAsyncResult *res = (GSimpleAsyncResult *) result;
+
+    g_return_val_if_fail (g_simple_async_result_is_valid (result,
+                G_OBJECT (node),
+                fmnode_set_property_async), FALSE);
+
+    g_simple_async_result_propagate_error (res, error);
+    return g_simple_async_result_get_op_res_gboolean (res);
+}
+
+static void
+get_valist (FsNode       *node,
+            GError      **error,
+            const gchar  *first_property_name,
+            va_list       va_args)
+{
+    GHashTable *props;
+    const gchar *name;
+
+    props = node->priv->props;
+    g_rw_lock_reader_lock (&node->priv->props_lock);
+    name = first_property_name;
+    while (name)
+    {
+        FsNodeProp *prop;
+        gchar *err;
+
+        prop = g_hash_table_lookup (props, (gpointer) name);
+        if (!prop)
+        {
+            g_set_error (error, FSNODE_ERROR, FSNODE_ERROR_NOT_FOUND,
+                    "Node does not have a property %s", name);
+            break;
+        }
+        if (!prop->has_value)
+        {
+            GError *err_local = NULL;
+
+            /* we remove the reader lock, to allow get_value to do its work
+             * and call set_prop, which needs a writer lock obviously */
+            g_rw_lock_reader_unlock (&node->priv->props_lock);
+            if (!prop->get_value (node, name, set_prop, &err_local))
+            {
+                g_propagate_error (error, err_local);
+                return;
+            }
+            g_rw_lock_reader_lock (&node->priv->props_lock);
+            /* let's assume prop is still valid. Properties cannot be removed,
+             * so it has to still exists after all */
+        }
+        G_VALUE_LCOPY (&(prop->value), va_args, 0, &err);
+        if (err)
+        {
+            g_set_error (error, FSNODE_ERROR, FSNODE_ERROR_OTHER,
+                    "Failed to get node property %s: %s",
+                    name,
+                    err);
+            g_free (err);
+            break;
+        }
+        name = va_arg (va_args, gchar *);
+    }
+    g_rw_lock_reader_unlock (&node->priv->props_lock);
+}
+
+void
+fsnode_get (FsNode       *node,
+            GError      **error,
+            const gchar  *first_property_name,
+            ...)
+{
+    va_list va_args;
+
+    g_return_if_fail (IS_FSNODE (node));
+
+    va_start (va_args, first_property_name);
+    get_valist (node, error, first_property_name, va_args);
+    va_end (va_args);
+}
+
+void
+fsnode_refresh (FsNode *node)
+{
+    GHashTableIter iter;
+    gpointer key, value;
+
+    g_return_if_fail (IS_FSNODE (node));
+    g_rw_lock_writer_lock (&node->priv->props_lock);
+    g_hash_table_iter_init (&iter, node->priv->props);
+    while (g_hash_table_iter_next (&iter, &key, &value))
+    {
+        ((FsNodeProp *) value)->has_value = FALSE;
+    }
+    g_rw_lock_writer_unlock (&node->priv->props_lock);
+}
+
+static void
+set_prop (FsNode        *node,
+          const gchar   *name,
+          GValue        *value)
+{
+    FsNodeProp *prop;
+
+    g_return_if_fail (IS_FSNODE (node));
+    g_return_if_fail (name != NULL);
+
+    g_rw_lock_writer_lock (&node->priv->props_lock);
+    prop = g_hash_table_lookup (node->priv->props, (gpointer) name);
+    if (prop)
+    {
+        g_value_copy (value, &(prop->value));
+        /* we assume it worked, w/out checking types, etc because this should
+         * only be used by providers and such, on properties they are handling,
+         * so if they get it wrong, they're seriously bugged */
+        prop->has_value = TRUE;
+    }
+    g_rw_lock_writer_unlock (&node->priv->props_lock);
+}
---
+++ fmnode.h
@@ -0,0 +1,96 @@
+
+#ifndef __FMNODE_H__
+#define __FMNODE_H__
+
+G_BEGIN_DECLS
+
+#define FMNODE_ERROR            g_quark_from_static_string ("FmNode-Error")
+typedef enum
+{
+    FMNODE_ERROR_NOMEM,
+    FMNODE_ERROR_ALREADY_EXISTS,
+    FMNODE_ERROR_NOT_FOUND,
+    FMNODE_ERROR_READ_ONLY,
+    FMNODE_ERROR_INVALID_TYPE,
+    FMNODE_ERROR_OTHER
+} FmNodeError;
+
+#define TYPE_FMNODE             (fmnode_get_type ())
+#define FMNODE(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), TYPE_FMNODE, FmNode))
+#define FMNODE_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), TYPE_FMNODE, FmNodeClass))
+#define IS_FMNODE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TYPE_FMNODE))
+#define IS_FMNODE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), TYPE_FMNODE))
+#define FMNODE_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), TYPE_FMNODE, FmNodeClass))
+
+typedef struct _FmNode          FmNode;
+typedef struct _FmNodeClass     FmNodeClass;
+typedef struct _FmNodePrivate   FmNodePrivate;
+
+/* functions called by a node to get/set a property value */
+typedef gboolean    (*get_value_fn) (FmNode         *node,
+                                     const gchar    *name,
+                                     GError        **error);
+typedef gboolean    (*set_value_fn) (FmNode         *node,
+                                     const gchar    *name,
+                                     GValue         *value,
+                                     GError        **error);
+
+struct _FmNode
+{
+    GObject parent;
+
+    FmNodePrivate *priv;
+};
+
+struct _FmNodeClass
+{
+    GObjectClass parent;
+};
+
+GType           fmnode_get_type             (void) G_GNUC_CONST;
+
+FmNode *        fmnode_new                  (FmProvider             *provider,
+                                             const gchar            *location,
+                                             gboolean                is_container);
+FmNode *        fmnode_new_from_node        (FmProvider             *provider,
+                                             const gchar            *location
+                                             gboolean                is_container,
+                                             FmNode                 *sce);
+FmProvider *    fmnode_get_provider         (FmNode                 *node);
+gchar *         fmnode_get_location         (FmNode                 *node);
+gboolean        fmnode_add_property         (FmNode                 *node,
+                                             const gchar            *name,
+                                             GType                   type,
+                                             GValue                 *value,
+                                             get_value_fn            get_value,
+                                             set_value_fn            set_value,
+                                             GError                **error);
+gboolean        fmnode_set_property         (FmNode                 *node,
+                                             const gchar            *name,
+                                             GValue                 *value,
+                                             GError                **error);
+void            fmnode_set_property_async   (FmNode                 *node,
+                                             const gchar            *name,
+                                             GValue                 *value,
+                                             GCancellable           *cancellable,
+                                             GAsyncReadyCallback     callback,
+                                             gpointer                data);
+gboolean        fmnode_set_property_finish  (FmNode                 *node,
+                                             GAsyncResult           *result,
+                                             GError                **error);
+void            fmnode_get                  (FmNode                 *node,
+                                             GError                **error,
+                                             const gchar            *first_name,
+                                             ...);
+void            fmnode_refresh              (FmNode                 *node);
+gboolean        fmnode_is_container         (FmNode                 *node);
+gchar *         fmnode_set_location         (FmNode                 *node,
+                                             const gchar            *new_location);
+void            fmnode_set_property_value   (FmNode                 *node,
+                                             const gchar            *name,
+                                             GValue                 *value);
+
+G_END_DECLS
+
+#endif /* __FMNODE_H__ */
+
