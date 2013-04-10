df5aa038ab242b0a43ed6ab6085345a0e26125c8
Olivier Brunel <jjk@jjacky.com>
2013-04-10 20:43:21 +0200

    Fix streq/strneq macros to handle NULL

--- macros.h
+++ macros.h
@@ -24,8 +24,10 @@ G_BEGIN_DECLS
 #define DONNA_LOG_DOMAIN    "Donnatella"
 #define donna_error(...)    g_log (DONNA_LOG_DOMAIN, G_LOG_LEVEL_WARNING, __VA_ARGS__)
 
-#define streq(s1, s2)           (strcmp  ((s1), (s2)) == 0)
-#define streqn(s1, s2, n)       (strncmp ((s1), (s2), (n)) == 0)
+#define streq(s1, s2)           (((s1) == NULL && (s2) == NULL) ? 1 \
+        : ((s1) == NULL || (s2) == NULL) ? 0 : strcmp  ((s1), (s2)) == 0)
+#define streqn(s1, s2, n)       (((s1) == NULL || (s2) == NULL) ? 0 \
+        : strncmp ((s1), (s2), (n)) == 0)
 
 G_END_DECLS
 
