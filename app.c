0c3b41055a77db0e8de7f09862aa409a607cc98e
Olivier Brunel <jjk@jjacky.com>
2013-12-03 20:12:48 +0100

    app: Fix new_int_ref() now allowing arrays
    Implementation in donna has been supporting them for a while though.

--- app.c
+++ app.c
@@ -499,7 +499,8 @@ donna_app_new_int_ref (DonnaApp       *app,
     g_return_val_if_fail (DONNA_IS_APP (app), NULL);
     g_return_val_if_fail (ptr != NULL, NULL);
     g_return_val_if_fail (type == DONNA_ARG_TYPE_TREEVIEW
-            || type == DONNA_ARG_TYPE_NODE, NULL);
+            || type == DONNA_ARG_TYPE_NODE
+            || (type & DONNA_ARG_IS_ARRAY), NULL);
 
     interface = DONNA_APP_GET_INTERFACE (app);
 
