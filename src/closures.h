334e15ee0133362f62e8f0ef3b1778e2e4b91ebf
Olivier Brunel <jjk@jjacky.com>
2014-02-04 20:36:23 +0100

    Add copyright headers

--- src/closures.h
+++ src/closures.h
@@ -1,3 +1,24 @@
+/*
+ * donnatella - Copyright (C) 2014 Olivier Brunel
+ *
+ * closures.h
+ * Copyright (C) 2014 Olivier Brunel <jjk@jjacky.com>
+ *
+ * This file is part of donnatella.
+ *
+ * donnatella is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * donnatella is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * donnatella. If not, see http://www.gnu.org/licenses/
+ */
 
 #ifndef __g_cclosure_user_marshal_MARSHAL_H__
 #define __g_cclosure_user_marshal_MARSHAL_H__
@@ -81,4 +102,3 @@ extern void g_cclosure_user_marshal_BOOLEAN__STRING_STRING_STRING_POINTER_POINTE
 G_END_DECLS
 
 #endif /* __g_cclosure_user_marshal_MARSHAL_H__ */
-
--- src/imagemenuitem.h
+++ src/imagemenuitem.h
@@ -1,3 +1,24 @@
+/*
+ * donnatella - Copyright (C) 2014 Olivier Brunel
+ *
+ * imagemenuitem.h
+ * Copyright (C) 2014 Olivier Brunel <jjk@jjacky.com>
+ *
+ * This file is part of donnatella.
+ *
+ * donnatella is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * donnatella is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * donnatella. If not, see http://www.gnu.org/licenses/
+ */
 
 #ifndef __DONNA_IMAGE_MENU_ITEM_H__
 #define __DONNA_IMAGE_MENU_ITEM_H__
